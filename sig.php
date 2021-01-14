a090f415792a76ccd2971ba3342bbdaa7a419656
yenifer <huamanrodascarolina@gmail.com>
2021-01-14 18:11:52 -0500

    actualizo los datos y selecciono lo que necesito y coloco estilos

--- sig.php
+++ sig.php
@@ -1,16 +1,6 @@
-<html>
-      <head> </head>
-      <body background="imagen/lud.jpg">
-      </body> 
-      </html>
-<?php 
-    $hn = 'localhost';
-    $db = 'tin';
-    $un = 'root';
-    $pw = '';
-    $port = 3306;
-?>
+
     <?php 
+    require_once 'Conexion.php';
     $conexion = new mysqli($hn, $un, $pw, $db, 3306);
     if ($conexion->connect_error) die ("Fatal error");
 
@@ -27,25 +17,22 @@
         echo $query;
         $result = $conexion->query($query);
         if (!$result) die ("Falló registro");
-        echo "<h1>Registro exitoso <a href='son.php'>Ingresar</a></h1>";
+        echo "<h1>Registro exitoso <a href='Login.php'>Ingresar</a></h1>";
         
     }
     else
     {
         echo <<<_END
-        <html>
-        <body > 
-        <form action="sig.php" method="post"><center>
-        <br></br>
-        <br></br>
-        <h1>Regístrate</h1><img src="imagen/log.jpg" style="width:70px;height:67px" />
-        <h3>Nombre  <input type="text" name="nombre" placeholder="Nombre" required=""></h3>
-        <h3>Apellido <input type="text" name="apellido" placeholder="Apellido" required="" ></h3>
-        <h3>Usuario  <input type="text" name="username" placeholder="username" required=""></h3>
-        <h3>Password <input type="text" name="password"  placeholder="Contraseña" required=""></h3> 
-        <h3>Email <input type="email" name="email" placeholder="Correo Electronico" required=""></h3></br>
+        <html><head> <link rel="stylesheet" href="logi.css"></head>
+         <div class="login">
+        <form action="sig.php" method="post"><center><h3>Regístrate</h3><img src="imagen/log.jpg" class="log"  style="width:70px;height:67px" />
+        <h3> <input type="text" name="nombre" placeholder="Nombre" required=""></h3>
+        <h3><input type="text" name="apellido" placeholder="Apellido" required="" ></h3>
+        <h3><input type="text" name="username" placeholder="username" required=""></h3>
+        <h3><input type="text" name="password"  placeholder="Contraseña" required=""></h3> 
+        <h3><input type="email" name="email" placeholder="Correo Electronico" required=""></h3></br>
                  <input type="hidden" name="reg" value="yes">
-                 <input type="submit" value="REGISTRAR" "></center></form></body></html>
+                 <input type="submit" value="REGISTRAR" style="width:100px;height:50px"></center></form></div></html>
         _END;
     }
     function mysql_entities_fix_string($conexion, $string)
