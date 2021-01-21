fc5053c4262bccc24727deef19a15459552d3cf8
yenifer <huamanrodascarolina@gmail.com>
2021-01-21 00:10:46 -0500

    puse los servicios que ofrecemos y su descripcion.

---
+++ servicio.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+	<title>Menu</title>
+    <link rel="stylesheet" href="estilo.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+<body>
+<header>
+    <div class="menu_bar">
+        <a href="#" >Menú</a>
+    </div>
+    <nav>
+    <ul>
+        <div id="header">
+            <ul class="nav">
+            <img  src="imagen/loo.jpg" alt="" width="70px" height="60px";>
+            <li><a href="index.html">Inicio</a>
+            <ul> 
+            <li ><a href="info.php">Informacion</a></li>
+            </ul>
+            </li>
+            <li ><a href="#">Productos</a>
+            <ul> 
+                <li ><h11><a href="registroP.php">Registro de Productos</a></h11></li>
+                 <li ><a href="diseñoPrecio.php">Descuentos en Producto</a></li>
+                    </ul></li>
+                <li><a href="categoria.php">Categoria Productos</a></li>
+                <li><a href="servicio.php">Servicios</a></li>
+           </ul>
+</div>
+</nav>
+</header>
+
+<center>
+<img src='imagen/cambio.jpg'  onmouseover="this.src='imagen/filtro.jpg'" onmouseout="this.src='imagen/cambio.jpg' " width="1500px" height="500px"; class="img-fluid">
+</center>            
+<body>
+<section id="imagen-titular">
+        </section>
+
+        <section id="main-section">
+            <article>
+            <h3> - Cambio de aceite </h3>
+            <p> Un cambio de aceite es simplemente el proceso de retirar el aceite usado.
+                Es habitualmente es una de las medidas más importantes que puede tomar para mantener el funcionamiento de su auto en buenas condiciones.así como para su duración. Con frecuencia, dentro de los gastos de mantenimiento habituales, esta prestación corresponde como mínimo a un 30 % del volumen de negocios de los mecánicos automovilísticos. Es decir, la parte principal de sus ingresos y, sin duda, de sus beneficios. Pero ¿realmente es necesario? ¿Para qué sirve? Y sobre todo,
+                 ¿cuánto cuesta realizar un cambio de aceite? He aquí la primera parte de nuestro informe “sin filtros” sobre el cambio de aceite.</p>
+                <img src='imagen/filt.jpg' onmouseover="this.src='imagen/filtrol.jpg'" onmouseout="this.src= 'imagen/filt.jpg' " width="900px" height="400px"; class="img-fluid">
+                 <h3>- Cambio de Filtro de aire </h3>
+                 <p> Por cada litro de combustible que gasta el vehículo hacen falta 10.000 litros de aire. Por todo ello, el filtro de aire debe cambiarse cada 15.000 kilómetros o una vez al año, lo que ocurra antes. No obstante, debido a condiciones ambientales y climatológicas, puede ser necesario cambiarlo antes de tiempo.</p>
+                 <img src='imagen/mecanica.jpg' onmouseover="this.src='imagen/filtro.jpg'" onmouseout="this.src='imagen/mecanica.jpg' " width="900px" height="400px"; class="img-fluid">
+                <h3>- Mantenimiento de Mecanica </h3>
+                <p> El mecánico de mantenimiento realiza el ajuste, instalación, revisión, acondicionamiento y reparación de motores, maquinaria agrícola e industrial y otros equipos mecánicos. Diagnosticar, reparar y ajustar distintos tipos de maquinaria, instalaciones y elementos mecánicos.</p>
+                <p>+Has reparacion Periodicamente y mante a tu vehiculo en buen estado. </p>
+                </p>+Lijas manuales; bloques de lijado y lijadora eléctrica.
+                +Cintas, hojas y tiras abrasivas; adhesivos y pegantes.</br> 
+                +Líneas de pulimento automotriz: para pegado y sellado.</br> 
+                +Discos de corte: (aspiración limpia, difuminados, de fibra, acabados, entre otros); respaldos suaves y soportes de discos.</p>
+            </article> 
+        </section>
+        <aside>
+            <div id="related-section">
+            <div class="related">
+                    <img src="imagen/color.jpg" width="60%" alt="">
+                    <h5>Color Rush.</h5>
+                </div>
+                <div class="related">
+                    <img src="imagen/max.jpeg" width="60%" alt="">
+                    <h5>Manner of Death. </h5>
+                </div>
+                <div class="related">
+                    <img src="imagen/TharnType.png" width="60%" alt="">
+                    <h5>TharnType.</h5>
+            </div>
+            </div>
+        </aside>
+        <style>
+		body{background: silver;}
+	    </style>
+        <footer>
+        </footer>
+    </div>
+</body>
+</html>
