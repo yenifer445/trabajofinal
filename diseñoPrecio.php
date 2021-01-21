e0bfb4ea40949b9d0b21e6ca8e22b064fd3ebd1d
yenifer <huamanrodascarolina@gmail.com>
2021-01-21 00:17:26 -0500

    hise mi tabla de precio de productos en descuento

---
+++ diseñoPrecio.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+	<title>Menu</title>
+    <link rel="stylesheet" href="estilo.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    </head>
+<body>
+<header>
+    <div class="menu_bar">
+        <a href="#" >Menú</a>
+    </div>
+    <nav>
+    <ul>
+        <div id="header">
+            <ul class="nav">
+            <img  src="imagen/loo.jpg" alt="" width="70px" height="60px";>
+            <li><a href="index.html">Inicio</a>
+            <ul> 
+            <li ><a href="info.php">Informacion</a></li>
+            </ul>
+            </li>
+            <li ><a href="#">Productos</a>
+            <ul> 
+                <li ><h11><a href="registroP.php">Registro de Productos</a></h11></li>
+                 <li ><a href="diseñoPrecio.php">Descuento Producto</a></li>
+                    </ul></li>
+                <li><a href="categoria.php">Categoria Productos</a></li>
+                <li><a href="servicio.php">Servicios</a></li>
+           </ul>
+</div>
+</nav>
+</header>
+
+    <head>
+        <link href="estiloP.css" rel="stylesheet" >
+        <link  href="main.css"   rel="stylesheet">
+    </head>
+    <body >
+    <section id="main-section">
+        <article>
+    <h1>Productos con Descuento</h1>
+    <p>Varía mucho el precio por la cantidad de pedidos y las más de doscientas referencias que se manejan en el negocio”, añade.
+    En cuanto a las ventas, el factor de los pedidos es muy importante, porque dependiendo de la cantidad de productos que requiera un cliente, se puede establecer un mejor precio y crear una red de compradores al por mayor.</p> 
+</article>
+</section>
+            
+    <div id="tabla-precios">
+    <div class="precio-col">
+    <div class="precio-col-header">
+    <h3>72$/caja x 2/140$ </h3>
+    <p>Esta en descuento</p>
+    </div>
+
+    <div class="precio-col-features">
+    <p>La hidrolina es un compuesto químico que sirve para el correcto funcionamiento del sistema hidráulico de 
+    nuestra dirección,es  importante para nuestro auto.</p>
+    <center><img  src="imagen/Lubricantes.png" alt="" width="205px" height="120px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+    <a>Comprar</a>
+    </div>
+    </div>
+
+    <div class="precio-col">
+    <div class="precio-col-header">
+    <h3>$/156</h3>
+    <p>Todos el producto en marca vistony cada uno 3 unidades cada uno dentro de la caja</p>
+    </div>
+
+    <div class="precio-col-features">
+     <p>Todos los aceites estan a mitad de precio es mejor aprobechar,como Liquido radiador, limpia parabrisas.</p>
+    <center><img  src="imagen/todo.jpg" alt="" width="200px" height="160px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+ <a>Comprar</a>
+ </div>
+ </div>
+
+<div class="precio-col">
+ <div class="precio-col-header">
+ <h3>$/350</h3>
+ <p>Estan en descuento</p>
+ </div>
+
+<div class="precio-col-features">
+ <p>Es el aceite lubricante para uso general en motores de 4T de bajas prestaciones,soporta muy altas temperaturas.</p>
+ <center><img  src="imagen/aceite.jpg" alt="" width="200px" height="150px";></center>
+ </div>
+
+<div class="precio-col-comprar">
+ <a >Comprar</a>
+ </div>
+</div>
+
+<div class="precio-col">
+    <div class="precio-col-header">
+    <h3>$/280</h3>
+    <p>Todos el producto en marca vistony viene 24 unidades cada uno dentro de la caja</p>
+    </div>
+
+    <div class="precio-col-features">
+     <p>Grasa vistony son grasa lubricante para un servicio multiple tanto para motos y autos.</p>
+    <center><img  src="imagen/gra.jpg" alt="" width="200px" height="160px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+ <a>Comprar</a>
+ </div>
+ </div>
+
+
+ <div class="precio-col">
+ <div class="precio-col-header">
+ <h3>$/149.25</h3>
+ <p>Pernos inoxidables</p>
+ </div>
+<div class="precio-col-features">
+ <p>  pieza metálica que puede tener diferentes tamaño. Es un elemento de unión.
+      perno es una pieza muy presente en el sector del automóvil.</p>
+ <center><img  src="imagen/pernos.png" alt="" width="200px" height="150px";></center>
+ </div>
+ <div class="precio-col-comprar">
+ <a >Comprar</a>
+ </div>
+</div>
+
+<div class="precio-col">
+    <div class="precio-col-header">
+    <h3>$/72</h3>
+    <p>Todos el producto en marca vistony cada uno 3 unidades cada uno dentro de la caja</p>
+    </div>
+
+    <div class="precio-col-features">
+     <p>Liquido radiador uno de los productos con un amplio benificio para los autos absorbiendo el calor del motor.</p>
+    <center><img  src="imagen/guuu.jpg" alt="" width="200px" height="160px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+ <a>Comprar</a>
+ </div>
+ </div>
+
+ <div class="precio-col">
+    <div class="precio-col-header">
+    <h3>$/70</h3>
+    <p>Los productos para reparar cuando la llanta o algun funcionamiento esta mal.</p>
+    </div>
+
+    <div class="precio-col-features">
+     <p>Martillo,destornillador entre otros productos para mantener bien al automovil.</p>
+    <center><img  src="imagen/img1.jpg" alt="" width="200px" height="160px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+ <a>Comprar</a>
+ </div>
+ </div>
+
+
+ <div class="precio-col">
+    <div class="precio-col-header">
+    <h3>$/65</h3>
+    <p>Todos el producto en marca vistony cada uno 3 unidades cada uno dentro de la caja</p>
+    </div>
+
+    <div class="precio-col-features">
+     <p>Todos los aceites estan a mitad de precio es mejor aprobechar,como Liquido para moto.</p>
+    <center><img  src="imagen/desc.jpg" alt="" width="210px" height="160px";></center>
+    </div>
+
+    <div class="precio-col-comprar">
+ <a>Comprar</a>
+ </div> 
+ </div></div>
+ <style>
+		body{background: silver;}
+	</style>
+</br></br></br>
+ <footer>
+ </footer>
+ </body>
+</html> 
